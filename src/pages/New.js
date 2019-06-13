02e3751ee7767cc030c06dbb86a4ee7ed96c5eb1
pclevy <pclevybr@gmail.com>
2019-06-13 18:00:56 -0300

    OmniStack7 - Frontend - finalizado

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,82 @@
 import React, { Component } from "react";
+import api from "../services/api";
+
+import "./New.css";
+import { throwStatement } from "@babel/types";
 
 class New extends Component {
+  state = {
+    image: null,
+    author: "",
+    place: "",
+    description: "",
+    hashtags: ""
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const data = new FormData();
+
+    data.append("image", this.state.image);
+    data.append("author", this.state.author);
+    data.append("place", this.state.place);
+    data.append("description", this.state.description);
+    data.append("hashtags", this.hashtags);
+
+    await api.post("posts", data);
+
+    this.props.history.push("/");
+
+    console.log(this.state);
+  };
+
+  handleImageChange = e => {
+    this.setState({ image: e.target.files[0] });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
-    return <h1>New</h1>;
+    return (
+      <form id="new-post" onSubmit={this.handleSubmit}>
+        <input type="file" onChange={this.handleImageChange} />
+
+        <input
+          type="text"
+          name="author"
+          placeholder="Autor do Post"
+          onChange={this.handleChange}
+          value={this.state.author}
+        />
+
+        <input
+          type="text"
+          name="place"
+          placeholder="Local do Post"
+          onChange={this.handleChange}
+          value={this.state.place}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Descrição do Post"
+          onChange={this.handleChange}
+          value={this.state.description}
+        />
+        <input
+          type="text"
+          name="hashtags"
+          placeholder="Hashtags do Post"
+          onChange={this.handleChange}
+          value={this.state.hashtags}
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
+    );
   }
 }
 
